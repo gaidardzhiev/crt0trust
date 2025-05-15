f040158507c3691096fc2d092d2d530644992848
gaidardzhiev <1v4n@protonmail.com>
2025-05-15 10:49:11 +0000

    Thu May 15 10:49:11 AM UTC 2025

---
+++ verify.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+TODO="write a verification script that uses strace to trace and analyze system calls and detect the execution of a shell-spawning payload injected by the compromised crt0.o"
+
+printf "%s\n" "$TODO"
