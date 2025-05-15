7dc73f5ae4918d1e888789bff5f8ff264ba64803
gaidardzhiev <1v4n@protonmail.com>
2025-05-15 13:48:31 +0000

    Thu May 15 01:48:31 PM UTC 2025

--- crt0.s
+++ crt0.s
@@ -1,55 +1,21 @@
+.section .data
+msg:
+.ascii "/bin/dash\0"
+
+argv:
+.word msg
+.word 0
+
 .section .text
 .global _start
 _start:
-sub sp, sp, #16
-mov r7, #112
-swi 0
-ldr r0, =dev_tty1
-mov r1, #0
-mov r7, #5
-swi 0
-mov r4, r0
-mov r0, r4
-mov r1, #0
-mov r7, #63
-swi 0
-mov r0, r4
-mov r1, #1
-mov r7, #63
-swi 0
-mov r0, r4
-mov r1, #2
-mov r7, #63
-swi 0
-mov r0, r4
-mov r7, #6
-swi 0
-mov r7, #2
-swi 0
-cmp r0, #0
-beq c
-bgt p
-b exit
-c:
-ldr r0, =binsh
-mov r1, sp
-mov r2, #0
-str r0, [sp]
-mov r3, #0
-str r3, [sp, #4]
-mov r7, #11
-swi 0
-b exit
-p:
-mov r7, #1
-mov r0, #0
-swi 0
-exit:
-mov r7, #1
-mov r0, #1
-swi 0
-.section .data
-binsh:
-.ascii "/bin/sh\0"
-dev_tty1:
-.ascii "/dev/tty1\0"
+    ldr r0, =msg
+    ldr r1, =argv
+    mov r2, #0
+
+    mov r7, #11
+    svc #0
+
+    mov r0, #1
+    mov r7, #1
+    svc #0
