5addfad0b7cd6c22f071838b9addabec127aff1f
gaidardzhiev <1v4n@protonmail.com>
2025-05-15 14:37:04 +0000

    Thu May 15 02:37:04 PM UTC 2025

--- crt0.s
+++ crt0.s
@@ -1,21 +1,48 @@
-.section .data
-msg:
-.ascii "/bin/dash\0"
-
-argv:
-.word msg
+.section .rodata
+.align 4
+b:
+.asciz "/bin/dash"
+a:
+.word b
 .word 0
-
 .section .text
 .global _start
+.extern main
 _start:
-    ldr r0, =msg
-    ldr r1, =argv
-    mov r2, #0
-
-    mov r7, #11
-    svc #0
-
-    mov r0, #1
-    mov r7, #1
-    svc #0
+ldr r4,[sp]
+add r5,sp,#4
+ldr r5,[r5]
+add r6,sp,#8
+ldr r6,[r6]
+mov r7,#2
+svc #0
+cmp r0,#0
+bgt parent
+child1:
+mov r0,#0
+mov r7,#112
+svc #0
+mov r7,#2
+svc #0
+cmp r0,#0
+bgt child1_exit
+child2:
+ldr r0,=b
+ldr r1,=a
+mov r2,#0
+mov r7,#11
+svc #0
+mov r0,#1
+mov r7,#1
+svc #0
+child1_exit:
+mov r0,#0
+mov r7,#1
+svc #0
+parent:
+mov r0,r4
+mov r1,r5
+mov r2,r6
+bl main
+mov r7,#1
+svc #0
