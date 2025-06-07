98826cbb1ba861c4cc9378380ae7ee9280a4787f
gaidardzhiev <1v4n@protonmail.com>
2025-06-07 09:35:18 +0000

    Sat Jun  7 09:35:18 AM UTC 2025

--- verify.sh
+++ verify.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-TODO="write a verification script that uses strace to trace and analyze system calls and detect the execution of a shell-spawning payload injected by the compromised crt0.o"
+TODO="write a verification script that uses strace to trace and analyze system calls and detect the execution of a reverse shell spawning payload injected by the compromised crt0.o"
 
-printf "%s\n" "$TODO"
+NC="on a separate terminal prepare the listener: nc -l -o 4444"
+
+REV="execute the reverse shell and analyze the syscalls: strace ./reverse_shell"
+
+printf "%s\n\n" "$TODO"
+
+printf "%s\n\n" "$NC"
+
+printf "%s\n\n" "$REV"
