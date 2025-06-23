028186adf7f1a426987758a3b3ff66c8835545c2
gaidardzhiev <1v4n@protonmail.com>
2025-06-23 07:26:13 +0000

    Mon Jun 23 07:26:13 AM UTC 2025

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 AS=as
 CC=gcc
 BIN=reverse_shell
+
 $(BIN): crt0.o main.o
 	$(CC) -static -nostdlib -e _start -o $(BIN) crt0.o main.o
+
 crt0.o: crt0.s
 	$(AS) -o crt0.o crt0.s
+
 main.o: main.c
 	$(CC) -c -o main.o main.c
+
 clean:
 	rm -f crt0.o main.o $(BIN)
--- verify.sh
+++ verify.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
-MSG="this is a verification script that uses strace to trace and analyze system calls and detect the execution of a reverse shell spawning payload injected by the compromised crt0.o"
-NC="prepare the listener on a separate terminal: nc -l -p 4444"
-LOG="/tmp/strace_output.log"
+W="/tmp/strace_output.log"
+X="this is a verification script that uses strace to trace and analyze system calls and detect the execution of a reverse shell spawning payload injected by the compromised crt0.o"
+Y="prepare the listener on a separate terminal: nc -l -p 4444"
+Z="press ENTER once listener is ready..."
 
-printf "%s\n\n" "$MSG" | pv -qL 30
+printf "%s\n\n" "$X" | pv -qL 30
 
-printf "\n%s\n\n" "$NC" | pv -qL 30
+printf "\n%s\n\n" "$Y" | pv -qL 30
 
-printf "press ENTER once listener is ready...\n\n"
+printf "%s\n\n" "$Z" | pv -qL 30
 
 read -r
 
 strace -f -s 1000 -e trace=execve,socket,connect,dup2 -o "$LOG" ./reverse_shell &
 
-STRACE_PID="$!"
+P="$!"
 
 sleep 10
 
-kill "$STRACE_PID" 2>/dev/null
+kill "$P" 2>/dev/null
 
-grep -E 'socket' "$LOG" && printf "socket syscall DETECTED...\n\n"
+grep -E 'socket' "$W" && printf "socket syscall DETECTED...\n\n"
 
-grep -E 'connect\(.*htons\([0-9]{2,5}\)' "$LOG" && printf "connect syscall DETECTED...\n\n"
+grep -E 'connect\(.*htons\([0-9]{2,5}\)' "$W" && printf "connect syscall DETECTED...\n\n"
 
-grep -iE 'execve\(".*(/?(sh|zsh|bash|dash))"' "$LOG" && printf "execvsyscall DETECTED...\n\n"
+grep -iE 'execve\(".*(/?(sh|zsh|bash|dash))"' "$W" && printf "execv syscall DETECTED...\n\n"
 
-grep -E 'dup2\([0-9]+, ?[0-2]\)' "$LOG" && printf "dup2 syscall DETECTED...\n\n"
+grep -E 'dup2\([0-9]+, ?[0-2]\)' "$W" && printf "dup2 syscall DETECTED...\n\n"
 
-rm "$LOG"
+rm "$W"
